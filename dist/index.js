0bc4173bfc601e6fa53a0dfb1ad09195250abd5b
sarfraz <mohammad.alam_int@external.swiggy.in>
2023-11-19 02:03:09 +0530

    update

--- dist/index.js
+++ dist/index.js
@@ -119807,6 +119807,76 @@ const generateTreeMap = async (bundle, sourcemap, filename) => {
   }
  };
 
+class TreeAnalyzer {
+    bundles = {
+        [branch_From] : treeMap[branch_From].bundles[0],
+        [branch_to] : treeMap[branch_to].bundles[0]
+    };
+    
+
+    TreeAnalyzer() {
+        this.bundles[branch_From] = treeMap[branch_From].bundles[0];
+        this.bundles[branch_to] = treeMap[branch_to].bundles[0];
+    }
+
+    createTree(files) {
+        if(!files) {
+            console.log("INVALID FILES TO CREATE TREE");
+            return;
+        }
+        const map = new Map();
+        Object.keys(files).map((f) => {
+            map.set(f, parseInt(files[f].size));
+        });
+
+        return map
+    }
+
+    findFileDiff(files, files_map = new Map()) {
+
+        const diff = {};
+
+        Object.keys(files).map((filePath) => {
+            if(!files_map.has(filePath)) {
+                diff[filePath] = -parseInt(files[filePath].size);
+            }else {
+                const size = parseInt(files[filePath].size);
+                const prev_size = files_map.get(filePath);
+
+                if(prev_size - size != 0) {
+                    diff[filePath] = prev_size - size;
+                }
+            }   
+            files_map.delete(filePath);
+        });
+
+        files_map.forEach((value, key) => {
+            diff[key] = value;
+        });
+
+        return diff;
+    }
+
+    analyze() {
+        const branchToMap = this.createTree(this.bundles[branch_to].files);
+
+        const totalBytesDifference = parseInt(this.bundles[branch_to].totalBytes) - parseInt(this.bundles[branch_From].totalBytes);
+        const mappedBytesDifference = this.bundles[branch_to].mappedBytes - this.bundles[branch_From].mappedBytes;
+        const unmappedBytesDifference = this.bundles[branch_to].unmappedBytes - this.bundles[branch_From].unmappedBytes;
+
+        const filesDiff = this.findFileDiff(this.bundles[branch_From].files, branchToMap);
+
+        return {
+            totalBytes : totalBytesDifference,
+            mappedBytes : mappedBytesDifference,
+            unmappedBytes : unmappedBytesDifference,
+            files : filesDiff
+        }
+    }
+
+
+}
+
 function getFiles(dir, files = []) {
   // Get an array of all files and directories in the passed directory using fs.readdirSync
   const fileList = fs$7.readdirSync(dir);
@@ -119854,7 +119924,8 @@ const branchBundler = async (branch_name) => {
 
   const files = [
     fileDetails.bundle,
-    fileDetails.source_map
+    fileDetails.source_map,
+    fileDetails.filename
   ];
 
   const rootDirectory = '.';
@@ -119902,38 +119973,28 @@ const analyzeBundler = async ({
 
   console.log(branch_From_map, branch_To_map);
 
-  treeMap[branch_From] = await generateTreeMap(
-    branch_From_map.bundle,
-    branch_From_map.source_map,
-    branch_From_map.filename
+  treeMap[branch_From] = JSON.parse(fs$7.readFileSync(branch_From_map.filename, 'utf8'));
+
+  treeMap[branch_to] = JSON.parse(fs$7.readFileSync(branch_To_map.filename, 'utf8'));
+
+  const treeAnalyzer = new TreeAnalyzer();
+
+  const res = treeAnalyzer.analyze();
+
+  fs$7.writeFileSync(
+      path$5.resolve('res.json'),
+      JSON.stringify(res)
   );
 
-  // treeMap[branch_to] = await generateTreeMap(
-  //   branch_To_map.bundle,
-  //   branch_To_map.source_map,
-  //   branch_To_map.filename
-  // );
+  const files = [
+    path$5.resolve('res.json')
+  ];
+  const rootDirectory = '.'; // Also possible to use __dirname
+  const options = {
+      continueOnError: false
+  };
 
-  // const treeAnalyzer = new TreeAnalyzer();
-
-  // const res = treeAnalyzer.analyze();
-
-  // console.log(res);
-
-  // fs.writeFileSync(
-  //     path.resolve('res.json'),
-  //     JSON.stringify(res)
-  // );
-
-  // const files = [
-  //   path.resolve('res.json')
-  // ]
-  // const rootDirectory = '.' // Also possible to use __dirname
-  // const options = {
-  //     continueOnError: false
-  // }
-
-  // await artifactClient.uploadArtifact('files', files, rootDirectory, options)
+  await artifactClient.uploadArtifact('files', files, rootDirectory, options);
 
 };
 
