06c97d086c8eec96f2f617756abee836c9d37fa0
sarfraz <mohammad.alam_int@external.swiggy.in>
2023-12-04 16:38:59 +0530

    update

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import _ from 'lodash'
+import dummy from "./dummy.json"
 
 export default function App() {
+
+  console.log(dummy)
+
   return (
     <View style={styles.container}>
       <Text>Welcome to Swiggy {_.camelCase("Hello World")}!</Text>
